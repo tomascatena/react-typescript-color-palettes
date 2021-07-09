08c4bbf571faf90f13edcf6e2c2e7e76fbffdabc
Tomás Catena <tomascatena@gmail.com>
2021-07-09 21:01:18 +0200

    fix palette route

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,46 @@
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Palette from './components/Palette/Palette';
 import seedPalettes from './seedPalettes';
 import './App.css';
 import { generatePalette } from './colorHelpers';
 
+interface ColorPalette {
+  paletteName: string;
+  id: string;
+  emoji: string;
+  colors: { name: string; color: string }[];
+}
+
 const App = (): JSX.Element => {
+  const findPalette = (id: string): ColorPalette => {
+    return seedPalettes.find((palette) => palette.id === id) as ColorPalette;
+  };
+
   return (
     <Switch>
       <Route exact path='/'>
         <h1>Palette List Goes Here</h1>
       </Route>
 
-      <Route exact path='/palette/:id'>
-        <div className='App'>
-          <Palette colorPalette={generatePalette(seedPalettes[2])} />
-        </div>
-      </Route>
+      <Route
+        exact
+        path='/palette/:id'
+        render={(routeProps) => {
+          if (!findPalette(routeProps.match.params.id)) {
+            return <Redirect to='/'></Redirect>;
+          } else {
+            return (
+              <div className='App'>
+                <Palette
+                  colorPalette={generatePalette(
+                    findPalette(routeProps.match.params.id)
+                  )}
+                />
+              </div>
+            );
+          }
+        }}
+      ></Route>
     </Switch>
   );
 };
