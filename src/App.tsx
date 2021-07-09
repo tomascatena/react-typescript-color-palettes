69c3ab83c718bc2c96fb03d4db77daa4799a49d8
Tomás Catena <tomascatena@gmail.com>
2021-07-09 20:40:52 +0200

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Route, Switch } from 'react-router-dom';
 import Palette from './components/Palette/Palette';
 import seedPalettes from './seedPalettes';
 import './App.css';
@@ -5,9 +6,17 @@ import { generatePalette } from './colorHelpers';
 
 const App = (): JSX.Element => {
   return (
-    <div className='App'>
-      <Palette colorPalette={generatePalette(seedPalettes[2])} />
-    </div>
+    <Switch>
+      <Route exact path='/'>
+        <h1>Palette List Goes Here</h1>
+      </Route>
+
+      <Route exact path='/palette/:id'>
+        <div className='App'>
+          <Palette colorPalette={generatePalette(seedPalettes[2])} />
+        </div>
+      </Route>
+    </Switch>
   );
 };
 
