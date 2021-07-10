a9b68d6951eaad63b0181da1fdd9b159b86f107b
Tomás Catena <tomascatena@gmail.com>
2021-07-10 19:47:34 +0200

    add Drawer to NewPaletteForm

--- src/components/NewPaletteForm/NewPaletteForm.tsx
+++ src/components/NewPaletteForm/NewPaletteForm.tsx
@@ -1,15 +1,87 @@
-import { withStyles } from '@material-ui/styles';
-import { WithStyles } from '@material-ui/core';
-import NewPaletteFormStyles from './NewPaletteFormStyles';
+import useStyles from './NewPaletteFormStyles';
+import { useState } from 'react';
+import clsx from 'clsx';
 
-interface NewPaletteFormProps extends WithStyles<typeof NewPaletteFormStyles> {}
+import { useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
-const NewPaletteForm = ({ classes }: NewPaletteFormProps): JSX.Element => {
+interface NewPaletteFormProps {}
+
+const NewPaletteForm = ({}: NewPaletteFormProps): JSX.Element => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div>
-      <h1>New Palette Form</h1>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position='fixed'
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            onClick={handleDrawerOpen}
+            edge='start'
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' noWrap>
+            Persistent drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant='persistent'
+        anchor='left'
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+      </main>
     </div>
   );
 };
 
-export default withStyles(NewPaletteFormStyles)(NewPaletteForm);
+export default NewPaletteForm;
