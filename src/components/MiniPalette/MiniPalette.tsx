bcb523f2eef4185cd609e41eb02f559800c51800
Tomás Catena <tomascatena@gmail.com>
2021-07-18 11:45:28 +0200

    complete transition between routes

--- src/components/MiniPalette/MiniPalette.tsx
+++ src/components/MiniPalette/MiniPalette.tsx
@@ -4,6 +4,7 @@ import { MouseEvent } from 'react';
 import { useHistory } from 'react-router-dom';
 import MiniPaletteStyles from './MiniPaletteStyles';
 import DeleteIcon from '@material-ui/icons/Delete';
+import React from 'react';
 
 interface MiniPaletteProps extends WithStyles<typeof MiniPaletteStyles> {
   paletteName: string;
@@ -13,50 +14,52 @@ interface MiniPaletteProps extends WithStyles<typeof MiniPaletteStyles> {
   openDialog: (id: string) => void;
 }
 
-const MiniPalette = ({
-  classes,
-  colors,
-  paletteName,
-  emoji,
-  id,
-  openDialog,
-}: MiniPaletteProps): JSX.Element => {
-  const history = useHistory();
+const MiniPalette = React.memo(
+  ({
+    classes,
+    colors,
+    paletteName,
+    emoji,
+    id,
+    openDialog,
+  }: MiniPaletteProps): JSX.Element => {
+    const history = useHistory();
 
-  const miniColorBoxes = colors.map(({ color, name }): JSX.Element => {
+    const miniColorBoxes = colors.map(({ color, name }): JSX.Element => {
+      return (
+        <div
+          className={classes.miniColorBox}
+          style={{ backgroundColor: color }}
+          key={name}
+        ></div>
+      );
+    });
+
+    const goToPalette = (e: MouseEvent<HTMLDivElement>, id: string): void => {
+      history.push(`/palette/${id}`);
+    };
+
+    const handleDeletePalette = (e: MouseEvent<HTMLDivElement>): void => {
+      e.stopPropagation();
+      openDialog(id);
+    };
+
     return (
-      <div
-        className={classes.miniColorBox}
-        style={{ backgroundColor: color }}
-        key={name}
-      ></div>
-    );
-  });
+      <div className={classes.root} onClick={(e) => goToPalette(e, id)}>
+        <div
+          className={classes.deleteIconContainer}
+          onClick={handleDeletePalette}
+        >
+          <DeleteIcon className={classes.deleteIcon} />
+        </div>
+        <div className={classes.colors}>{miniColorBoxes}</div>
 
-  const goToPalette = (e: MouseEvent<HTMLDivElement>, id: string): void => {
-    history.push(`/palette/${id}`);
-  };
-
-  const handleDeletePalette = (e: MouseEvent<HTMLDivElement>): void => {
-    e.stopPropagation();
-    openDialog(id);
-  };
-
-  return (
-    <div className={classes.root} onClick={(e) => goToPalette(e, id)}>
-      <div
-        className={classes.deleteIconContainer}
-        onClick={handleDeletePalette}
-      >
-        <DeleteIcon className={classes.deleteIcon} />
+        <h5 className={classes.title}>
+          {paletteName} <span className={classes.emoji}>{emoji}</span>
+        </h5>
       </div>
-      <div className={classes.colors}>{miniColorBoxes}</div>
-
-      <h5 className={classes.title}>
-        {paletteName} <span className={classes.emoji}>{emoji}</span>
-      </h5>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default withStyles(MiniPaletteStyles)(MiniPalette);
